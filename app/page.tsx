c1c612993607374051c17cfdc0b6963e25da1369
Oliver Kelso <oliver@pixelsoul.com>
2024-12-09 17:16:23 -0600

    init commit for nextjs version

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Card, CardContent } from "@/components/ui/card"
+
+export default function Home() {
+    return (
+        <Card>
+            <CardContent>
+                <p className="text-2xl">Hello World</p>
+            </CardContent>
+        </Card>
+    )
+}
