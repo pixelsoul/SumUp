54d2b896bcc301eee87b2e85d378155dc7aec7aa
Oliver Kelso <oliver@pixelsoul.com>
2024-12-16 17:44:21 -0600

    some style changes. update footer.

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,20 +1,21 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { Separator } from "@/components/ui/separator"
 
 const siteName = process.env.NEXT_PUBLIC_SITE_NAME
 
 const Footer = () => {
     return (
-        <footer className=" py-4 flex flex-col items-center justify-center mt-auto">
+        <footer className=" py-4 flex flex-row gap-4 items-center justify-center mt-auto">
             <p>&copy; 2024 {siteName}</p>
-            <Button
-                variant="ghost"
-                type="button"
-                onClick={() => window.open("https://github.com/pixelsoul/ai-summarizer")}
-                className="black_btn">
-                Github
-            </Button>
+            <Separator orientation="vertical" className="h-4" />
+            <p>
+                Developed By:{" "}
+                <Link href="https://pixelsoul.com" target="_blank">
+                    pixelsoul.com
+                </Link>
+            </p>
         </footer>
     )
 }
